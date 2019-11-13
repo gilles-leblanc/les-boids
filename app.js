39f10f9696a334bb8d323495b8ab2108695a38d7
Gilles Leblanc <gilles.leblanc@gmail.com>
2019-11-12 21:38:45 -0500

    Make the boids move in a straigth line

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var Point = (function () {
         this.x = x;
         this.y = y;
     }
+    Point.prototype.translate = function (tx, ty) {
+        return new Point(this.x - tx, this.y - ty);
+    };
     return Point;
 }());
 var Boid = (function () {
@@ -13,43 +16,80 @@ var Boid = (function () {
         this.point = new Point(x, y);
         this.angle = angle;
     }
+    Boid.prototype.move = function () {
+        this.point = this.point.translate(this.speed, this.speed);
+    };
     return Boid;
 }());
-function drawBoid(boid, context) {
-    var sideLength = 20;
-    var p1 = { x: boid.point.x, y: boid.point.y };
-    var p2 = { x: boid.point.x + sideLength, y: boid.point.y };
-    var p3 = { x: boid.point.x + sideLength, y: boid.point.y + sideLength };
-    context.translate(boid.point.x, boid.point.y);
-    context.rotate(boid.angle);
-    context.translate(-boid.point.x, -boid.point.y);
-    context.moveTo(p1.x, p1.y);
-    context.lineTo(p2.x, p2.y);
-    context.lineTo(p3.x, p3.y);
-    context.lineTo(p1.x, p1.y);
-    context.lineWidth = 1;
-    context.fillStyle = '#8ED6FF';
-    context.fill();
-    context.strokeStyle = '#666666';
-    context.stroke();
-    context.setTransform(1, 0, 0, 1, 0, 0);
-}
-function start() {
-    console.log('started');
-    var boids = [];
-    var canvas = document.getElementById('main-canvas');
-    if (canvas) {
-        var context_1 = canvas.getContext('2d', { alpha: false });
-        var canvasWidth = canvas.getBoundingClientRect().width;
-        var canvasHeight = canvas.getBoundingClientRect().height;
-        canvas.width = canvasWidth;
-        canvas.height = canvasHeight;
-        if (context_1) {
-            context_1.fillStyle = "white";
-            context_1.fillRect(0, 0, canvasWidth, canvasHeight);
-            boids.push(new Boid(100, 100, Math.PI / 4));
-            boids.forEach(function (b) { return drawBoid(b, context_1); });
+var BoidWorld = (function () {
+    function BoidWorld() {
+        this.canvasHeight = 0;
+        this.canvasWidth = 0;
+        this.context = null;
+        this.boids = [];
+        var canvas = document.getElementById('main-canvas');
+        if (canvas) {
+            this.context = canvas.getContext('2d', { alpha: false });
+            this.canvasWidth = canvas.getBoundingClientRect().width;
+            this.canvasHeight = canvas.getBoundingClientRect().height;
+            canvas.width = this.canvasWidth;
+            canvas.height = this.canvasHeight;
         }
     }
-}
-start();
+    BoidWorld.prototype.clear = function () {
+        if (this.context) {
+            this.context.fillStyle = "white";
+            this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+        }
+    };
+    BoidWorld.prototype.drawBoid = function (boid) {
+        if (!this.context) {
+            return;
+        }
+        var sideLength = 20;
+        var p1 = { x: boid.point.x, y: boid.point.y };
+        var p2 = { x: boid.point.x + sideLength, y: boid.point.y };
+        var p3 = { x: boid.point.x + sideLength, y: boid.point.y + sideLength };
+        this.context.translate(boid.point.x, boid.point.y);
+        this.context.rotate(boid.angle);
+        this.context.translate(-boid.point.x, -boid.point.y);
+        this.context.beginPath();
+        this.context.moveTo(p1.x, p1.y);
+        this.context.lineTo(p2.x, p2.y);
+        this.context.lineTo(p3.x, p3.y);
+        this.context.lineTo(p1.x, p1.y);
+        this.context.lineWidth = 1;
+        this.context.fillStyle = '#8ED6FF';
+        this.context.fill();
+        this.context.strokeStyle = '#666666';
+        this.context.stroke();
+        this.context.closePath();
+        this.context.setTransform(1, 0, 0, 1, 0, 0);
+    };
+    BoidWorld.prototype.updateBoids = function (timestamp) {
+        var _this = this;
+        if (!this.startStamp) {
+            this.startStamp = timestamp;
+        }
+        if (this.context) {
+            this.clear();
+            this.boids.forEach(function (b) { return b.move(); });
+            this.boids.forEach(function (b) { return _this.drawBoid(b); });
+        }
+        if (timestamp - this.startStamp < 2000) {
+            requestAnimationFrame(this.updateBoids.bind(this));
+        }
+    };
+    BoidWorld.prototype.create = function () {
+        var _this = this;
+        this.clear();
+        this.boids.push(new Boid(100, 100, Math.PI / 4));
+        this.boids.push(new Boid(20, 500, 0));
+        this.boids.push(new Boid(304, 450, Math.PI / 2));
+        this.boids.forEach(function (b) { return _this.drawBoid(b); });
+        requestAnimationFrame(this.updateBoids.bind(this));
+    };
+    return BoidWorld;
+}());
+var boidWorld = new BoidWorld();
+boidWorld.create();
--- app.ts
+++ app.ts
@@ -7,6 +7,10 @@ class Point {
     this.x = x;
     this.y = y;
   }
+
+  translate(tx: number, ty: number): Point {
+    return new Point(this.x - tx, this.y - ty);
+  }
 }
 
 class Boid {
@@ -19,64 +23,104 @@ class Boid {
     this.point = new Point(x, y);
     this.angle = angle;
   }
-}
 
-function drawBoid(boid: Boid, context: CanvasRenderingContext2D) {
-  const sideLength = 20;
-
-  // get points from triangular boid
-  const p1 = { x: boid.point.x, y: boid.point.y };
-  const p2 = { x: boid.point.x + sideLength, y: boid.point.y };
-  const p3 = { x: boid.point.x + sideLength, y: boid.point.y + sideLength };
-   
-  // apply rotation to correct angle
-  context.translate(boid.point.x, boid.point.y);
-  context.rotate(boid.angle);
-  context.translate(-boid.point.x, -boid.point.y);
-  
-  // draw boid
-  context.moveTo(p1.x, p1.y);
-  context.lineTo(p2.x, p2.y);
-  context.lineTo(p3.x, p3.y);
-  context.lineTo(p1.x, p1.y);  
-  
-  context.lineWidth = 1;
-  context.fillStyle = '#8ED6FF';
-  context.fill();
-  context.strokeStyle = '#666666';
-  context.stroke();
-  
-  // reset transform matrix
-  context.setTransform(1, 0, 0, 1, 0, 0)
-}
-
-function start() {
-  console.log('started');
-  let boids: Array<Boid> = [];
-
-  const canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
-  
-  if (canvas) {
-    const context = canvas.getContext('2d', { alpha: false });
-    const canvasWidth = canvas.getBoundingClientRect().width;
-    const canvasHeight = canvas.getBoundingClientRect().height;
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-
-    if (context) {
-      context.fillStyle = "white";
-      context.fillRect(0, 0, canvasWidth, canvasHeight);
-      
-      // create some boids
-      boids.push(new Boid(100, 100, Math.PI / 4)); // Math.PI / 4
-    
-      // render them
-      boids.forEach(b => drawBoid(b, context));
-    
-      // update them
-
-    }
+  move() {
+    this.point = this.point.translate(this.speed, this.speed);
   }
 }
 
-start();
+class BoidWorld {
+  canvasHeight = 0;
+  canvasWidth = 0;
+  context: CanvasRenderingContext2D | null = null;
+  startStamp: number | undefined;
+  boids: Array<Boid> = [];  
+
+  constructor() {
+    const canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
+    
+    if (canvas) {
+      this.context = canvas.getContext('2d', { alpha: false });
+      this.canvasWidth = canvas.getBoundingClientRect().width;
+      this.canvasHeight = canvas.getBoundingClientRect().height;
+      canvas.width = this.canvasWidth;
+      canvas.height = this.canvasHeight;
+    }
+  }
+
+  clear() {
+    if (this.context) {
+      this.context.fillStyle = "white";
+      this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+    }
+  }
+
+  drawBoid(boid: Boid) {
+    if (!this.context) {
+      return;
+    }
+
+    const sideLength = 20;
+  
+    // get points from triangular boid
+    const p1 = { x: boid.point.x, y: boid.point.y };
+    const p2 = { x: boid.point.x + sideLength, y: boid.point.y };
+    const p3 = { x: boid.point.x + sideLength, y: boid.point.y + sideLength };
+     
+    // apply rotation to correct angle
+    this.context.translate(boid.point.x, boid.point.y);
+    this.context.rotate(boid.angle);
+    this.context.translate(-boid.point.x, -boid.point.y);
+    
+    // draw boid
+    this.context.beginPath();
+    this.context.moveTo(p1.x, p1.y);
+    this.context.lineTo(p2.x, p2.y);
+    this.context.lineTo(p3.x, p3.y);
+    this.context.lineTo(p1.x, p1.y);  
+    
+    this.context.lineWidth = 1;
+    this.context.fillStyle = '#8ED6FF';
+    this.context.fill();
+    this.context.strokeStyle = '#666666';
+    this.context.stroke();
+    this.context.closePath();
+    
+    // reset transform matrix
+    this.context.setTransform(1, 0, 0, 1, 0, 0)
+  }
+
+  updateBoids(timestamp: number) {
+    if (!this.startStamp) {
+      this.startStamp = timestamp;
+    }
+  
+    if (this.context) {
+      this.clear();
+      this.boids.forEach(b => b.move());
+      this.boids.forEach(b => this.drawBoid(b));
+    }
+  
+    if (timestamp - this.startStamp < 2000) {
+      requestAnimationFrame(this.updateBoids.bind(this));
+    }
+  }
+
+  create() {
+    this.clear();
+
+    // create some boids
+    this.boids.push(new Boid(100, 100, Math.PI / 4));
+    this.boids.push(new Boid(20, 500, 0));
+    this.boids.push(new Boid(304, 450, Math.PI / 2));
+  
+    // render them
+    this.boids.forEach(b => this.drawBoid(b));
+  
+    // update them
+    requestAnimationFrame(this.updateBoids.bind(this));
+  }
+}
+
+let boidWorld = new BoidWorld();
+boidWorld.create();
